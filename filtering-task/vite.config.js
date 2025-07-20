d8af935510087c78531e053b153945def7330472
unknown <zenahmad06@gmail.com>
2025-07-20 04:27:38 -0700

    update config

--- filtering-task/vite.config.js
+++ filtering-task/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'/filtering-task/',
+  base:'/all-project/filtering-task/',
   plugins: [react()],
 })
